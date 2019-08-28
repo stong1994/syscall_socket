0baf15f12ab40fb4080f68455223ac5ded49a810
stong1994 <st5983@outlook.com>
2019-08-28 10:01:42 +0700

    init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module tcp_server
+
+go 1.12
+
+require github.com/google/gopacket v1.1.17
---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"tcp_server/client"
+	"tcp_server/server"
+	"time"
+)
+
+func main() {
+	sender()
+	// reciver()
+	//client.BuildPacket()
+}
+
+func reciver(){
+	server.Run()
+}
+
+func sender(){
+	//if exec,closeExec,err := client.Send("93.177.80.131", 8888, "212.22.73.9", 21, "ac fun"); err != nil {
+	if exec, closeExec, err := client.Send("127.0.0.1",22,"154.208.143.31",9999,"ac fun");err != nil{
+		fmt.Println("err is :", err)
+	}else{
+		defer closeExec()
+		for{
+			if err := exec() ;err != nil{
+				fmt.Println("running err is: ",err)
+				time.Sleep(2*time.Second)
+			}
+		}
+	}
+}
